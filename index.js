107371b3dd8e657817fa529db3e7d11498c5bace
aarena18 <ambrearena@gmail.com>
2023-04-04 15:46:19 +0200

    :alembic: test functions

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-gsap.to('.circle',{
-    x : -300,
-    duration : 1,
-    repeat : -1,
-    yoyo : true,
-})
+gsap.to(".circle", {
+  x: -300,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+});
+
+const btnLumos = document.querySelector(".sorts2");
+const btnStop = document.querySelector(".sorts3");
+btnStop.addEventListener("click", stopAnimation);
+
+const stopAnimation = () => {
+  lumos.pause();
+};
